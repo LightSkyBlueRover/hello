8fbaaade408047579af912bec72a26ef9f73b70d
Robert <robertpschmidt42@gmail.com>
2025-08-28 12:08:21 -0500

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Name: Robert Schmidt
+ * Date: 2025/08/28
+ *
+ * Learning Github and making a print commanded
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("I am a Computer Engineer Major\n");
+
+  return 0;
+}
+
