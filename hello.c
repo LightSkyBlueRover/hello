1832e447287095fd432f12cdaac7bc2e1317ef2b
Ty <tydresden2@huskers.unl.edu>
2025-09-02 11:26:58 -0500

    added my name and major to output

---
+++ hello.c
@@ -0,0 +1,22 @@
+/**
+ * Name: Robert Schmidt
+ * Date: 2025/08/28
+ *
+ * Learning Github and making a command that prints my name and major
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("My name is Robert!\n");
+  printf("I am a Computer Engineering Major\n");
+
+  // Prints Ty's name and major into Robert's code 2025/09/02
+
+  printf("My name is Ty\n");
+  printf("I am a Computer Science Major\n");
+
+  return 0;
+}
